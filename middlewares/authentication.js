209eed94177a2cbfbec7a190dd129c971bb996eb
dondifer <dondifer@gmail.com>
2023-05-22 20:22:51 +0200

    feat: is autho commented

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -40,4 +40,25 @@ const isAdmin = async (req, res, next) => {
   next();
 };
 
+// const isAuthor = async (req, res, next) => {
+//   try {
+//     const order = await Order.findById(req.params._id);
+
+//     if (order.userId.toString() !== req.user._id.toString()) {
+//       return res
+//         .status(403)
+//         .send({ message: "Quieto paro! Este post no es el tuyo" });
+//     }
+
+//     next();
+//   } catch (error) {
+//     console.error(error);
+
+//     return res.status(500).send({
+//       error,
+//       message: "Ha habido un problema al editar o borrar el post",
+//     });
+//   }
+// };
+
 module.exports = { authentication, isAdmin };
