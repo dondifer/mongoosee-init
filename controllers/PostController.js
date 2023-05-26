87f3dd2d37245ba3ffcf06f82f35d87379545216
dondifer <dondifer@gmail.com>
2023-05-26 18:22:53 +0200

    Create update and delete post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -13,6 +13,29 @@ const PostController = {
       console.error(error);
     }
   },
+  async update(req, res) {
+    try {
+      const post = await Post.findByIdAndUpdate(
+        req.params._id,
+        { ...req.body, userId: req.user._id },
+        {
+          new: true,
+        }
+      );
+      console.log(req.params._id);
+      res.send({ message: "Se ha actualizado perfecstamiente", post });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  async delete(req, res) {
+    try {
+      const post = await Post.findByIdAndDelete(req.params._id);
+      res.send({ message: "Éxito al borrar el post", post });
+    } catch (error) {
+      console.error(error);
+    }
+  },
 };
 
 module.exports = PostController;
--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,7 @@ const PostController = require("../controllers/PostController");
 const { authentication, isAdmin } = require("../middlewares/authentication");
 
 router.post("/", authentication, PostController.create);
+router.put("/update/:_id", authentication, PostController.update);
+router.delete("/delete/:_id", authentication, PostController.delete);
 
 module.exports = router;
