6107ec49bc6556b3c506215088d06b1ebdc3c143
dondifer <dondifer@gmail.com>
2023-07-08 10:23:57 +0200

    d

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const UserController = require("../controllers/UserController");
 const { authentication } = require("../middlewares/authentication");
 const { setImage } = require("../middlewares/imageUpload");
 
-router.post("/", upload.any(), setImage, UserController.register);
+router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/info", authentication, UserController.getInfo);
 router.delete("/logout", authentication, UserController.logout);
